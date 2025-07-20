7b8ea633e94340615da58e6ec1414ea8bf9bc875
HONGSOKHEARCAMBOBIDA <emmma7096@gmail.com>
2025-07-20 17:11:39 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
         <div class="logo-title">
- <img :src="`${process.env.https://hongsokhearcambobida.github.io/vueproject}logo.png`" />
+ <img :src="`${hongsokhearcambobida.github.io/vueproject}logo.png`" />
     <h1>ក្រុមហ៊ុន កាក់ ឯ.ក KAG Co., LTD</h1>
   </div>
     <nav>
